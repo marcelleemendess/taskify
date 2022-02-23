99563dfb5016db8079b345681265758bab84948b
Marcelle Mendes <marcelleemendess@mymac.local>
2022-02-22 19:00:57 -0700

    update app with interfaces

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import './App.css';
+import InputField from './componets/InputField';
+import { Todo } from './model';
+
+const App:React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+    console.log(todo);
+    
+  return (
+    <div className="App">
+      <span className="heading">Taskify</span>
+      <InputField 
+      todo={todo} 
+      setTodo={setTodo}
+      /> 
+    </div>
+  );
+};
+
+export default App;
