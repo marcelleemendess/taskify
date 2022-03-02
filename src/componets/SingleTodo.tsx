f023f58ef05f145610d4bad6934003e95f3c3771
Marcelle Mendes <marcelleemendess@mymac.local>
2022-03-01 17:43:51 -0700

    adding func. to the btns

--- src/componets/SingleTodo.tsx
+++ src/componets/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Todo } from '../model';
 import { AiFillEdit, AiFillDelete } from 'react-icons/ai';
 import { MdDone } from 'react-icons/md';
@@ -11,19 +11,44 @@ interface Props {
 }
 
 const SingleTodo: React.FC<Props>= ({todo, todos, setTodos}) => {
-  
+    const [edit, setEdit] = useState<boolean>(false)
+    const [editTodo, setEditTodo] = useState<string>(todo.todo)
+
     const handleDone = (id:number) => {
-        todos.map((todo) => todo.id === id ? {...todo, isDone:!todo.isDone} : todo)
+        setTodos(
+            todos.map((todo) => todo.id === id ? {...todo, isDone:!todo.isDone} 
+            : 
+            todo)
+        )
+    }
+
+    const handleDelete = (id:number) => {
+        const newTodos = todos.filter((todo) => todo.id !== id );
+        setTodos(newTodos);
+    }
+
+    const handleEdit = (id:number) => {
+        setEdit(!edit)
     }
   
     return (
     <form className="todos__single"> 
-        <span className="todos__sigle--text">{todo.todo}</span>
+        { todo.isDone ? (
+            <s className="todos__sigle--text">{todo.todo}</s>
+        ) : (
+            <span className="todos__sigle--text">{todo.todo}</span>
+        )}
         <div>
-            <span className="icon">
+            <span 
+                className="icon" 
+                onClick={() =>{
+                    if(!edit && !todo.isDone){
+                        setEdit(!edit)
+                    }
+                }}>
                 <AiFillEdit/> 
             </span>
-            <span className="icon">
+            <span className="icon" onClick={() => handleDelete(todo.id)}>
                 <AiFillDelete/>
             </span>
             <span className="icon" onClick={() => handleDone(todo.id)}>
